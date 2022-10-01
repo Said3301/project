92531f82095ca6bf8b414ef68d56af09b4f5bb45
sss <trat3301@gmail.com>
2022-10-01 09:04:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,541 @@
+"use strict"
+
+// let NameIs = "Name is "
+// let AgeIs = "Age is "
+
+// // const obj = {
+// //     a: 50
+// // };
+// // obj.a = 10;
+// // console.log(obj)
+
+// let obj = {
+//     names:{},
+//     ages:{}
+// }
+
+// let clientNameNum1 = "John"
+// let clientAgeNum1 = 25
+
+// // let expertObj = {
+// //     expertNames:{},
+// //     expertAges:{}
+// // }
+
+// // expertObj.expertNames = (obj.names = clientNameNum1)
+// // expertObj.expertAges = (obj.ages = clientAgeNum1)
+// obj.names = clientNameNum1
+// obj.ages = clientAgeNum1
+
+// // alert(NameIs+expertObj.expertNames+'\n'+ AgeIs+expertObj.expertAges)
+
+// alert(obj.names+"\n"+obj.ages)
+
+// -------------------------------------
+
+// let fnk = {
+//     fnk: function text(){
+
+//     }
+// }
+
+// let arr = ['a','b','c']
+
+// arr[10] = '3456'
+// console.log(arr)
+
+// let arrObj = {
+//     0:'a',
+//     '1':'b',
+//     2:'c',
+//     abc:{
+//         df:[{},{}],
+//         def:{
+
+//         }
+//     }
+// }
+// let b
+
+// arrObj.b = '1234'
+// arrObj['b'] = '1234'
+// arrObj[b] = '1234'
+// arrObj[b] = []
+// console.log(arrObj['b'])
+// console.log(arrObj[b])
+// console.log(arrObj.b)
+
+// -------------------------------------
+
+// alert('hello')
+// const result = confirm('Are you here')
+// console.log(result)
+
+// const answer = +prompt("Are you 18","18")
+// console.log(answer+5)
+
+// const answers = []
+// // answers[0] = prompt("what's your name")
+// // answers[1] = prompt("what's your surname")
+// // answers[2] = prompt("what's your name")
+
+// console.log(typeof(answers))
+
+// -------------------------------------
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++
+// decr--
+// // ==
+// ++incr
+// --decr
+
+// // Это неправильно
+
+// console.log(incr++)
+// console.log(decr--)
+
+// // -.-.-.-.-.-.-.-
+
+// console.log(incr)
+// console.log(decr)
+
+// console.log(2*4 == '8')
+
+// // this is false
+// console.log(2*4 === '8')
+// // -.-.-.-.-.-.-.-
+
+// console.log(4 + +'5')
+
+// Покажет остаток деление
+
+// console.log(5%2)
+
+// // -.-.-.-.-.-.-.-
+
+// const isChecked = true,
+//       isClose = false;
+// // false
+// console.log(isChecked && isClose)
+
+// // true
+// console.log(isChecked && isChecked)
+
+// // || true
+// console.log(isChecked || isClose)
+
+// // || false
+// console.log(isClose || isClose)
+
+// // -.-.-.-.-.-.-.-
+
+// console.log(isClose || !isClose)
+// console.log(2+2*2 !== "6")
+
+// -------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
